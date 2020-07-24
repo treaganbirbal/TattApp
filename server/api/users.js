6c5f1549cfd0964b7074e846ed5330b4dd8d2f9f
Treagan Birbal <treaganbirbal@pursuit.org>
2020-07-23 20:38:44 -0400

    chore: create api route to post new user

--- server/api/users.js
+++ server/api/users.js
@@ -1,17 +1,39 @@
-const router = require('express').Router()
-const {User} = require('../db/models')
-module.exports = router
+const router = require("express").Router();
+const { User } = require("../db/models");
+module.exports = router;
 
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
-    const users = await User.findAll({
+    const users = await User
+      .findAll
       // explicitly select only the id and email fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
-      attributes: ['id', 'email']
-    })
-    res.json(users)
+      //   attributes: ["id", "email", ""],
+      ();
+    res.json(users);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const newUser = await User.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password,
+      userType: req.body.userType,
+    });
+    if (newUser) {
+      res
+        .status(200)
+        .json({ message: "User Created Successfully ", user: newUser });
+    } else {
+      res.sendStatus(500);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
---
+++ server/db/models/artPiece.js
@@ -0,0 +1,9 @@
+const crypto = require("crypto");
+const Sequelize = require("sequelize");
+const db = require("../db");
+
+
+
+module.exports = {
+    artPiece
+}
