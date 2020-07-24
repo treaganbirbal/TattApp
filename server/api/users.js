827f7bf35443b34351022924035d68332b9e59cd
Treagan Birbal <treaganbirbal@pursuit.org>
2020-07-23 21:09:06 -0400

    chore: create api routes to get, update, and delete user

--- server/api/users.js
+++ server/api/users.js
@@ -17,6 +17,51 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+router.get("/:id", async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+    const user = await User.findOne({
+      where: { id: userId },
+    });
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+    await User.update(
+      {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password,
+      },
+      { where: { id: userId } }
+    );
+    if (!userId) {
+      res.sendStatus(500);
+    } else {
+      const updatedUser = await User.findOne({ where: { id: userId } });
+      if (updatedUser) {
+        res
+          .status(200)
+          .json({ message: "User updated successfully", user: updatedUser });
+      } else {
+        res.sendStatus(404);
+      }
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
 router.post("/", async (req, res, next) => {
   try {
     const newUser = await User.create({
@@ -32,6 +77,33 @@ router.post("/", async (req, res, next) => {
         .json({ message: "User Created Successfully ", user: newUser });
     } else {
       res.sendStatus(500);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+    if (!userId) {
+      res.sendStatus(500);
+    } else {
+      const user = await User.findOne({
+        where: { id: userId },
+      });
+      if (!user) {
+        res.sendStatus(404);
+      } else {
+        User.destroy({
+          where: {
+            id: userId,
+          },
+        });
+        res.status(200).json({
+          message: "User removed successfully",
+        });
+      }
     }
   } catch (error) {
     next(error);
--- server/db/models/artPiece.js
+++ server/db/models/artPiece.js
@@ -2,8 +2,6 @@ const crypto = require("crypto");
 const Sequelize = require("sequelize");
 const db = require("../db");
 
-
-
 module.exports = {
-    artPiece
-}
+  artPiece,
+};
